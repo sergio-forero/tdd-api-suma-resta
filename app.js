a111a9509cb17fbf0fbfc9018172c2c93d08bfb7
Sergio Forero <sergio.forero@avaldigitallabs.com>
2021-04-27 08:50:03 -0500

    Sonar and build actions added

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ app = express();
 
 app.use(validacion);
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/add', require('./controller/add'));
 app.get('/subtract', require('./controller/subtract'));
 
-app.listen(3000, ()=>{
-console.log('App is listening - localhost:3000');
+app.listen(app.get('port'), ()=>{
+    console.log('App is listening - localhost:' + app.get('port'));
 });
 
 module.exports = app;
